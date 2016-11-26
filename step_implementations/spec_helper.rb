2e1ac15cdd91f83f0c247c1faa2bf9165d3cbac4
Ashwin Gonsalves <ashwingonsalves@gmail.com>
2016-11-26 16:25:09 +0530

    Update spec_helper.rb

--- step_implementations/spec_helper.rb
+++ step_implementations/spec_helper.rb
@@ -1,4 +1,7 @@
 require_relative "./pages/base_page.rb"
+
+require 'selenium/webdriver'
+
 Dir[File.join(File.dirname(__FILE__), './pages/*.rb')].each {|file| require file }
 
 
@@ -31,8 +34,32 @@ end
 # https://github.com/jnicklas/capybara#drivers
 # Firefox 35 + Selenium has a bug - https://code.google.com/p/selenium/issues/detail?id=8390
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+# LOCAL RUN
+# Capybara.register_driver :selenium do |app|
+#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
+# end
+
+# Capybara.default_driver = :selenium
+
+# BROWSERSTACK
+Capybara.register_driver :browserstack do |app|
+  caps = Selenium::WebDriver::Remote::Capabilities.new
+  caps['os'] = "Windows"
+  caps['os_version'] = "7"
+  caps['browser'] = "Chrome"
+  caps['browser_version'] = "53.0"
+  caps['resolution'] = "1280x800"
+  caps['browserstack.local'] = "true"
+  caps['browserstack.debug'] = "true"
+
+  caps['build'] = "BrowserStack Gauge Framework"
+  caps['project'] = "BrowserStack Gauge Framework"
+
+  Capybara::Selenium::Driver.new(app,
+    :browser => :remote,
+    :url => "http://<BROWSERSTACK_USERNAME>:<BROWSERSTACK_ACCESS_KEY>@hub.browserstack.com/wd/hub",
+    :desired_capabilities => caps
+  )
 end
 
-Capybara.default_driver = :selenium
+Capybara.default_driver = :browserstack
